d8f3085dfa4ae805befcbe9ef800509b44ec1d35
willplantikow <mwillianx@gmail.com>
2025-07-04 19:39:04 -0300

    Implemente o vetor

---
+++ vetor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Quantos numeros voce deseja inserir no vetor? ";
+    cin >> n;
+
+    vector<int> vetor(n);
+
+    for (int i = 0; i < n; ++i) {
+        cout << "Digite o " << i + 1 << "o numero: ";
+        cin >> vetor[i];
+    }
+
+    if (n > 0) {
+        int maior_valor = vetor[0];
+
+        for (int i = 1; i < n; ++i) {
+            if (vetor[i] > maior_valor) {
+                maior_valor = vetor[i];
+            }
+        }
+        cout << "O maior valor no vetor e: " << maior_valor << endl;
+    } else {
+        cout << "O vetor esta vazio." << endl;
+    }
+
+    return 0;
+}
