545b769950587522a45a198c12c2900d054ab65f
willplantikow <mwillianx@gmail.com>
2025-07-04 20:58:59 -0300

    Incluindo Teste primo

--- generate-test-primo.sh
+++ generate-test-primo.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 NUM=$(( RANDOM % 1000 + 1 ))
 echo $NUM > test-primo.in
-echo "Gerado teste: $NUM"
+echo "Gerado test-primo.in com o número: $NUM"
+if (( NUM <= 1 )); then
+    EXPECTED_OUTPUT="Não é primo"
+elif (( NUM == 2 || NUM == 3 )); then
+    EXPECTED_OUTPUT="Primo"
+else
+    IS_PRIME="Primo"
+    for (( i=2; i*i <= NUM; i++ )); do
+        if (( NUM % i == 0 )); then
+            IS_PRIME="Não é primo"
+            break
+        fi
+    done
+    EXPECTED_OUTPUT="$IS_PRIME"
+fi
+echo "$EXPECTED_OUTPUT" > test-primo.out
+echo "Saída esperada salva em test-primo.out: $EXPECTED_OUTPUT"
