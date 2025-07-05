ad004715801fefd640db757ebe24166ab609914d
willplantikow <mwillianx@gmail.com>
2025-07-04 22:18:01 -0300

    alteração no generate Matriz

--- generate-test-matriz.sh
+++ generate-test-matriz.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
-N=$(( RANDOM % 4 + 2 ))
+
+N=$(( (RANDOM % 4) + 2 ))
 echo $N > test-matriz.in
 
+echo "Primeira Matriz (${N}x${N}):"
 for (( i=0; i<N; i++ )); do
+    ROW_OUTPUT=""
     for (( j=0; j<N; j++ )); do
-        echo -n "$(( RANDOM % 10 )) " >> test-matriz.in
+        RANDOM_NUM=$(( RANDOM % 10 ))
+        echo -n "$RANDOM_NUM " >> test-matriz.in
+        ROW_OUTPUT="$ROW_OUTPUT$RANDOM_NUM "
     done
     echo "" >> test-matriz.in
+    echo "$ROW_OUTPUT"
 done
 
+echo ""
+
+echo "Segunda Matriz (${N}x${N}):"
 for (( i=0; i<N; i++ )); do
+    ROW_OUTPUT=""
     for (( j=0; j<N; j++ )); do
-        echo -n "$(( RANDOM % 10 )) " >> test-matriz.in
+        RANDOM_NUM=$(( RANDOM % 10 ))
+        echo -n "$RANDOM_NUM " >> test-matriz.in
+        ROW_OUTPUT="$ROW_OUTPUT$RANDOM_NUM "
     done
     echo "" >> test-matriz.in
+    echo "$ROW_OUTPUT"
 done
 
 OPERATIONS=("a" "s" "m")
 OP_INDEX=$(( RANDOM % 3 ))
-echo "${OPERATIONS[$OP_INDEX]}" >> test-matriz.in
-echo "test-matriz.in com matrizes ${N}x${N} e operação ${OPERATIONS[$OP_INDEX]}."
+echo "${OPERATIONS[OP_INDEX]}" >> test-matriz.in
+
+echo ""
+echo "test-matriz.in com matrizes ${N}x${N} e operação ${OPERATIONS[OP_INDEX]}."
