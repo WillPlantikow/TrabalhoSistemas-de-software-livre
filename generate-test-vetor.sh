42753bcc262a0d4b769d4c17f5f0231a223ffd76
willplantikow <mwillianx@gmail.com>
2025-07-04 21:08:42 -0300

    Editando teste de vetor

--- generate-test-vetor.sh
+++ generate-test-vetor.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 N=$(( RANDOM % 15 + 1 ))
 echo $N > test-vetor.in
-echo "Gerado teste Tamanho $N e os seguintes elementos:"
+echo "Gerado test-vetor.in com tamanho $N."
+echo "Elementos gerados:"
+MAIOR_ESPERADO=-2147483648
 for (( i=0; i<N; i++ )); do
     ELEMENT=$(( RANDOM % 1001 ))
     echo $ELEMENT >> test-vetor.in
     echo -n "$ELEMENT "
+    if (( ELEMENT > MAIOR_ESPERADO )); then
+        MAIOR_ESPERADO=$ELEMENT
+    fi
 done
 echo ""
+echo "$MAIOR_ESPERADO" > test-vetor.out
+echo "test-vetor.out: $MAIOR_ESPERADO"
