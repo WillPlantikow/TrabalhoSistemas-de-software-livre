487422f18d9e637c99e894f9e7b54c8353d35f02
willplantikow <mwillianx@gmail.com>
2025-07-04 21:15:50 -0300

    Editando teste de matriz

--- generate-test-matriz.sh
+++ generate-test-matriz.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-N=$(( RANDOM % 15 + 1 ))
-echo $N > test-vetor.in
-echo "Gerado test-vetor.in com tamanho $N."
-echo "Elementos gerados:"
-MAIOR_ESPERADO=-2147483648
+N=$(( RANDOM % 4 + 2 ))
+echo $N > test-matriz.in
+
 for (( i=0; i<N; i++ )); do
-    ELEMENT=$(( RANDOM % 1001 ))
-    echo $ELEMENT >> test-vetor.in
-    echo -n "$ELEMENT "
-    if (( ELEMENT > MAIOR_ESPERADO )); then
-        MAIOR_ESPERADO=$ELEMENT
-    fi
+    for (( j=0; j<N; j++ )); do
+        echo -n "$(( RANDOM % 10 )) " >> test-matriz.in
+    done
+    echo "" >> test-matriz.in
 done
-echo ""
-echo "$MAIOR_ESPERADO" > test-vetor.out
-echo "test-vetor.out: $MAIOR_ESPERADO"
+
+for (( i=0; i<N; i++ )); do
+    for (( j=0; j<N; j++ )); do
+        echo -n "$(( RANDOM % 10 )) " >> test-matriz.in
+    done
+    echo "" >> test-matriz.in
+done
+
+OPERATIONS=("a" "s" "m")
+OP_INDEX=$(( RANDOM % 3 ))
+echo "${OPERATIONS[$OP_INDEX]}" >> test-matriz.in
+echo "test-matriz.in com matrizes ${N}x${N} e operação ${OPERATIONS[$OP_INDEX]}."
